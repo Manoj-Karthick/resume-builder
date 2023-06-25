27f5916c4dbe3532b9585230e905d6591c04659f
Manoj <manojkarthick1706@gmail.com>
2023-06-25 21:52:15 +0530

    Added basic navigation using Tailwind CSS

--- components/navbar.js
+++ components/navbar.js
@@ -1,9 +1,32 @@
 export default function NavBar() {
   return (
-    <div className="py-4 border-b border-slate-950/10 mx-4">
-      <div className="max-w-7xl  mx-auto px-4">
-        <div className="relative flex items-center">
-          <div className="font-bold">Resume</div>
+    <div className="mx-auto max-w-container px-4 sm:px-6 lg:px-8 font-semibold">
+      <div className="relative flex items-center py-4">
+        <div className="mr-auto px-3 text-slate-900 hover:cursor-pointer">
+          Resume
+        </div>
+        <div className="hidden lg:flex lg:items-center">
+          <div className="hover:cursor-pointer">Resume</div>
+          <div className="ml-8 hover:cursor-pointer">Cover Letter</div>
+          <div className="hidden lg:ml-8 lg:pl-8 lg:flex lg:items-center lg:border-l lg:border-slate-300">
+            <div className="inline-flex justify-center px-4 py-2.5 text-sm text-white font-semibold border bg-slate-950 rounded-lg hover:bg-slate-700 hover:cursor-pointer -my-2.5">
+              <span className="pr-2">Create an account</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
+                />
+              </svg>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,14 @@ import NavBar from "@/components/navbar";
 
 function HomePage() {
   return (
-    <div className="container">
+    <div>
       <NavBar />
-      <h1 className="text-9xl font-bold">Resume Builder Project</h1>
+      <div className="container mx-auto px-4 bg-gray-100">
+        <div className="columns-2 px-2">
+          <div className="w-full px-2 py-2">Resume Display</div>
+          <div className="w-full px-2 py-2">Resume Editor</div>
+        </div>
+      </div>
     </div>
   );
 }
